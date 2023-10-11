cc808dc2230a12fe1af97d484cad5b02ab0269ec
Ryan Alves <ra546038@gmail.com>
2023-10-11 17:30:59 -0300

    Ajustes

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -15,7 +15,45 @@ let project = [
         figma: 'https://www.figma.com/file/lfhuuXMmv9WVn4yyYs4Uor/Harry-Potter?type=design&node-id=0%3A1&mode=design&t=nPPzwnHYPWPXps39-1',
         github: 'https://github.com/RyanAlvesz/harry_potter',
         site: 'https://ryanalvesz.github.io/harry_potter/'
+    },
+    {
+        student: 'Julia Paiva',
+            class: 'ds1ait-b',
+            project_name: 'Studio Ghibli Movie',
+            project_image: 'https://github.com/Paivaas/StudioGhibli/blob/main/movie/img/Studio%20Ghibli%20Movie.png?raw=true',
+            figma: 'https://www.figma.com/file/Wit1TMZuRWQVbYGNrZJ4tp/Page-Studio-Ghibli?type=design&node-id=0-1&mode=design&t=3qoi5ejtA5rbcgYF-0',
+            github: 'https://github.com/Paivaas/StudioGhibli',
+            site: 'https://paivaas.github.io/StudioGhibli/'
+    },
+    {
+        student: 'Pedro Pedraga',
+            class: 'ds1ait-b',
+            project_name: 'Clash Royale APi',
+            project_image: 'https://user-images.githubusercontent.com/124139371/274326490-6588b3ff-5f3c-4a32-823b-6025f42e2b5d.png',
+            figma: 'https://www.figma.com/file/4jy6Z40MDonIr1xktudQM5/Untitled?type=design&node-id=1-3&mode=design&t=zNiInWfG6oija7c5-0',
+            github: 'https://github.com/PedrooTz/ClashRoyaleAPI',
+            site: 'https://pedrootz.github.io/ClashRoyaleAPI/'
+    },
+    {
+        student: 'Gabriela Fernandes',
+            class: 'ds1ait-b',
+            project_name: 'Marvel Characters & Comics',
+            project_image: 'https://user-images.githubusercontent.com/124157058/274400312-924d88d4-fa3a-43b4-bb26-48f3d2235933.png',
+            figma: 'https://www.figma.com/file/rygsayOYqoFatqlNt2rqYP/marvel-api?type=design&node-id=0%3A1&mode=design&t=XgP7q3AcKphImgHH-1',
+            github: 'https://github.com/gabfernandes8/marvel-pwfe-api',
+            site: 'https://gabfernandes8.github.io/marvel-pwfe-api/'
+    },
+    {
+        student: 'Matheus Zanoni',
+            class: 'ds1ait-b',
+            project_name: 'Anime Database',
+            project_image: 'https://user-images.githubusercontent.com/123570240/274327290-23581b49-77be-41ea-8d7a-bf88d205771d.png',
+            figma: 'https://www.figma.com/file/GELQawofprZQKRoqP0mNk1/Untitled?type=design&node-id=0-1&mode=design&t=My2yXQzUf2qdip8b-0',
+            github: 'https://github.com/Mat3uss/api-anime',
+            site: 'https://mat3uss.github.io/api-anime/'
     }
+    
+    
     
 ]
 
